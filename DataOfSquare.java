2d7b71866f026503dba7ac898bd48f800e60f53e
Pratiksha Patil <67996947+Pratik04sha@users.noreply.github.com>
2020-11-11 09:16:24 +0530

    Snake game
    A java based simple beginner program for a small snake game.

---
+++ DataOfSquare.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.awt.Color;
+
+public class DataOfSquare {
+
+
+    //ArrayList that'll contain the colors
+    ArrayList<Color> C =new ArrayList<Color>();
+    int color; //2: snake , 1: food, 0:empty
+    SquarePanel square;
+    public DataOfSquare(int col){
+
+        //Lets add the color to the arrayList
+        C.add(Color.green);//0
+        C.add(Color.RED);    //1
+        C.add(Color.yellow);   //2
+        color=col;
+        square = new SquarePanel(C.get(color));
+    }
+    public void lightMeUp(int c){
+        square.ChangeColor(C.get(c));
+    }
+}
